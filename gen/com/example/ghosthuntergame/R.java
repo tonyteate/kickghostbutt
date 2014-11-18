37043030ed4af89c97952476ab0dd9562bfccae0
bradleybarrett <bbarrett3160@gmail.com>
2014-11-18 18:08:07 -0500

    make sure changes to GameView for adding ghost to screen went through

--- gen/com/example/ghosthuntergame/R.java
+++ gen/com/example/ghosthuntergame/R.java
@@ -23,7 +23,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int down_button_image=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ghost_object_image=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f080007;
