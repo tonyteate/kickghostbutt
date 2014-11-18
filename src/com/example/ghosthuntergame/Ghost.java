4fd90bb52cd06fc56b4b8e655e6546e8e77c80bb
bradleybarrett <bbarrett3160@gmail.com>
2014-11-18 17:54:29 -0500

    ghosts will appear on screen at position (100,100) with intial velocity of 1 in x and y directions. Ghost is added to arraylist of onscreenobjects in onDraw method og gameView class.

--- src/com/example/ghosthuntergame/Ghost.java
+++ src/com/example/ghosthuntergame/Ghost.java
@@ -3,12 +3,14 @@ package com.example.ghosthuntergame;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 
+//want ghost 
+
 public class Ghost extends GamePiece {
 
-	public Ghost(int id, int xP, int yP, int height, int width,
-			Bitmap sourceImage) {
+	public Ghost(int id, int xP, int yP, int height, int width,Bitmap sourceImage) {
 		super(id, xP, yP, height, width, sourceImage);
-		// TODO Auto-generated constructor stub
+		this.setxVelocity(1);
+		this.setyVelocity(1);
 	}
 
 
